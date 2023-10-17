cab5aab120b221939d5aa054eb9b13035dc147c5
yanina-penayo <yan.a.pe@hotmail.com>
2023-10-17 10:22:24 -0300

    sprint2-productos

--- src/js/app-productos.js
+++ src/js/app-productos.js
@@ -4,3 +4,20 @@ const btn = document.querySelector('.menu-btn');
 btn.addEventListener('click', () => {
     menu.classList.toggle('active')
 })
+
+
+document.querySelectorAll('.button').forEach(button => {
+    button.addEventListener('click', function() {
+      const card = this.closest('.card');
+      card.querySelector('.front').style.display = 'none';
+      card.querySelector('.back').style.display = 'block';
+    });
+  });
+  
+  document.querySelectorAll('.back').forEach(back => {
+    back.addEventListener('click', function() {
+      const card = this.closest('.card');
+      card.querySelector('.front').style.display = 'block';
+      card.querySelector('.back').style.display = 'none';
+    });
+  });
